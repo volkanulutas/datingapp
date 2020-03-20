ae4a7b5ea7263e685f2a18330732fdc84ee845ee
volkanulutas <volkanulutas@hacettepe.edu.tr>
2020-03-20 14:27:20 +0300

    Config server is added.

---
+++ datingapp-config-server/src/main/java/com/datingapp/configserver/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.datingapp.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Datingapp on 19.03.2020
+ *
+ * @author volkanulutas
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
---
+++ datingapp-config-server/src/test/java/com/datingapp/configserver/ConfigServerApplicationTest.java
@@ -0,0 +1,21 @@
+package com.datingapp.configserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Datingapp on 19.03.2020
+ *
+ * @author volkanulutas
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigServerApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
