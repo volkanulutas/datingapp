e9e672013d670797af9975464d6625a970884870
onurcan <onurcanyozgat@gmail.com>
2020-03-21 23:48:53 +0300

    - data sınıfları template olarak eklendi

---
+++ datingapp-game-service/src/main/java/com/datingapp/game/data/Answer.java
@@ -0,0 +1,16 @@
+package com.datingapp.game.data;
+
+/**
+ * Created on 21.03.2020
+ *
+ * @author onurcanyozgat
+ */
+
+public class Answer {
+
+    private int id;
+    private int questionId;
+    private int participantId;
+    private String answerContent;
+
+}
---
+++ datingapp-game-service/src/main/java/com/datingapp/game/data/Participant.java
@@ -0,0 +1,17 @@
+package com.datingapp.game.data;
+
+import java.util.List;
+
+/**
+ * Created on 21.03.2020
+ *
+ * @author onurcanyozgat
+ */
+
+public class Participant {
+
+    private int id;
+    private String nickname;
+    private List<ParticipantFeature> featureList;
+
+}
